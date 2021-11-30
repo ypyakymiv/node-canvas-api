9e5a2d383fe4db9d6ef17115ff2bc501309fb8f4
Yuri Yakymiv <ypyakymiv@gmail.com>
2021-11-30 16:09:08 -0500

    added lists to getAnnouncements

--- src/getAnnouncements.js
+++ src/getAnnouncements.js
@@ -8,6 +8,10 @@ require('dotenv').config();
 
 const canvasDomain = process.env.CANVAS_API_DOMAIN;
 
+function stringifyDate(d) {
+  return `${d.getFullYear()}-${("0" + String(d.getMonth() + 1)).slice(-2)}-${("0" + d.getDate()).slice(-2)}`
+}
+
 /**
  * Retrieves all rubrics in course
  * @param {Number} courseId the course id.
@@ -16,6 +20,9 @@ const canvasDomain = process.env.CANVAS_API_DOMAIN;
  */
 
 function getAnnouncements(token, canvasDomain, courseId, ...options) {
-  return fetchAll(token, canvasDomain + '/api/v1' + `/announcements?context_codes[]=${courseId}&latest_only=true&` + buildOptions([getOptions.rubric.assessments, options]));
+  const oneYearAgo = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+  const today = new Date();
+
+  return fetchAll(token, "http://" + canvasDomain + '/api/v1' + `/announcements?context_codes[]=course_${courseId}&end_date=${stringifyDate(today)}&start_date=${stringifyDate(oneYearAgo)}&latest_only=false&active_only=true&` + buildOptions([getOptions.rubric.assessments, options]));
 }
 module.exports = getAnnouncements;
